dcd3847f1cd96c2fc3dcc555cc858de33b53ff8f
irfan <irfanjmi29@gmail.com>
2023-05-01 03:40:17 +0530

    first commit

---
+++ src/components/data/Data.js
@@ -0,0 +1,320 @@
+export const nav = [
+  {
+    text: "home",
+    path: "/",
+  },
+  {
+    text: "about",
+    path: "/about",
+  },
+  {
+    text: "services",
+    path: "/services",
+  },
+  {
+    text: "blog",
+    path: "/blog",
+  },
+  {
+    text: "pricing",
+    path: "/pricing",
+  },
+  {
+    text: "contact",
+    path: "/contact",
+  },
+]
+export const featured = [
+  {
+    cover: "../images/hero/h1.png",
+    name: "Family House",
+    total: "122 Property",
+  },
+  {
+    cover: "../images/hero/h2.png",
+    name: "House & Villa",
+    total: "155 Property",
+  },
+  {
+    cover: "../images/hero/h3.png",
+    name: "Apartment",
+    total: "300 Property",
+  },
+  {
+    cover: "../images/hero/h4.png",
+    name: "Office & Studio",
+    total: "80 Property",
+  },
+  {
+    cover: "../images/hero/h6.png",
+    name: "Villa & Condo",
+    total: "80 Property",
+  },
+]
+export const list = [
+  {
+    id: 1,
+    cover: "../images/list/p-1.png",
+    name: "Irfan  Real Estate",
+    location: "Okhla, New Delhi",
+    category: "For Rent",
+    price: " 35k",
+    type: "Apartment",
+  },
+  {
+    id: 2,
+    cover: "../images/list/p-2.png",
+    name: "Jamian Properties",
+    location: "South Delhi, Delhi",
+    category: "For Sale",
+    price: "30k",
+    type: "India",
+  },
+  {
+    id: 3,
+    cover: "../images/list/p-7.png",
+    name: "The Modi Estate Corner",
+    location: "Surat, India",
+    category: "For Rent",
+    price: "50k",
+    type: "Offices",
+  },
+  {
+    id: 4,
+    cover: "../images/list/p-4.png",
+    name: "Virat Kohli Realty",
+    location: "Banglor, India",
+    category: "For Sale",
+    price: "25k",
+    type: "Homes & Villas",
+  },
+  {
+    id: 5,
+    cover: "../images/list/p-5.png",
+    name: "SRK Realty",
+    location: "Mannat Lands(Bandra West), Mumbai",
+    category: "For Rent",
+    price: "90k",
+    type: "Commercial",
+  },
+  {
+    id: 6,
+    cover: "../images/list/p-6.png",
+    name: "Jaipur Tree Realty",
+    location: "Near WTP, Jaipur",
+    category: "For Sale",
+    price: "75k",
+    type: "Apartment",
+  },
+]
+export const awards = [
+  {
+    icon: <i class='fa-solid fa-trophy'></i>,
+    num: "32 M	",
+    name: "Jamia  Burmin Award",
+  },
+  {
+    icon: <i class='fa-solid fa-briefcase'></i>,
+    num: "43 M",
+    name: "IIT X11 Award",
+  },
+  {
+    icon: <i class='fa-solid fa-lightbulb'></i>,
+    num: "51 M",
+    name: "Indian UGC Award",
+  },
+  {
+    icon: <i class='fa-solid fa-heart'></i>,
+    num: "42 M",
+    name: "New Delhi Green Award",
+  },
+]
+export const location = [
+  {
+    id: 1,
+    name: "Noida,UP",
+    Villas: "Noida",
+    Apartments: "10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/dlf.png",
+  },
+  {
+    id: 2,
+    name: "Mumbai, India",
+    Villas: "12 Villas",
+    Apartments: "10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/l&t.png",
+  },
+  {
+    id: 3,
+    name: "Valley Kevadiya,India",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/l&t2.png",
+  },
+  {
+    id: 4,
+    name: "Jaipur, Rajsthan",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/dlf.png",
+  },
+  {
+    id: 5,
+    name: "Kanpur, UP",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-5.png",
+  },
+  {
+    id: 6,
+    name: " Kota, Rajsthanng",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/c2.png",
+  },
+]
+export const team = [
+  {
+    list: "50",
+    
+    cover: "../images/customer/IMG_5441.jpg",
+    
+    address: "New Frinds Colony, South Delhi",
+    name: "Irfan Khan",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "70",
+    cover: "../images/customer/faisal1.jpg",
+    address: "Batala House, India",
+    name: "Faisal Khan",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "80",
+    cover: "../images/customer/alziyan.jpg",
+    address: "Batla House",
+    name: "Alziyan",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "51",
+    cover: "../images/customer/gunu.jpg",
+    address: "Jaipur, India",
+    name: "Michael P. Grimaldo",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "42",
+    cover: "../images/customer/IMG_5441.jpg",
+    address: "NFC, New Delhi",
+    name: "Irfan",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "38",
+    cover: "../images/customer/IMG_5441.jpg",
+    address: "New Frinds colony, New DElhi",
+    name: "Irfan Khan",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+]
+export const price = [
+  {
+    plan: "Basic",
+    price: "29",
+    ptext: "per user, per month",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "99.5% Uptime Guarantee",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "120GB CDN Bandwidth",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "5GB Cloud Storage",
+      },
+      { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Personal Help Support" },
+      { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Enterprise SLA" },
+    ],
+  },
+  {
+    best: "Best Value",
+    plan: "Standard",
+    price: "49",
+    ptext: "per user, per month",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "99.5% Uptime Guarantee",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "150GB CDN Bandwidth",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "10GB Cloud Storage",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Personal Help Support",
+      },
+      {
+        change: "color",
+        icon: <i class='fa-solid fa-x'></i>,
+        text: "Enterprise SLA",
+      },
+    ],
+  },
+  {
+    plan: "Platinum",
+    price: "79",
+    ptext: "2 user, per month",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "100% Uptime Guarantee",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "200GB CDN Bandwidth",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "20GB Cloud Storage",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Personal Help Support",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Enterprise SLA",
+      },
+    ],
+  },
+]
+export const footer = [
+  {
+    title: "LAYOUTS",
+    text: [{ list: "Home Page" }, { list: "About Page" }, { list: "Service Page" }, { list: "Property Page" }, { list: "Contact Page" }, { list: "Single Blog" }],
+  },
+  {
+    title: "ALL SECTIONS",
+    text: [{ list: "Headers" }, { list: "Features" }, { list: "Attractive" }, { list: "Testimonials" }, { list: "Videos" }, { list: "Footers" }],
+  },
+  {
+    title: "COMPANY",
+    text: [{ list: "About" }, { list: "Blog" }, { list: "Pricing" }, { list: "Affiliate" }, { list: "Login" }, { list: "Changelog" }],
+  },
+]
